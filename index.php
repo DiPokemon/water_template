bd16d49038a42fbd74aa6be54f08f7f0887a54dc
DiPokemon <dimitrnklnk@gmail.com>
2022-11-13 23:40:24 +0300

    main slider + price section

--- index.php
+++ index.php
@@ -1,3 +1,63 @@
 <?php get_header(); ?>
 
+<section class="main_slider-block">
+    <div class="main_slider-wrapper">  
+
+        <div class="main_slide-item" style="background-image: url('/static/img/ustanovka-schetchikov-vody.png')">
+            <div class="main_slide-body container">
+                <div class="main_slide-left">
+                    <div class="main_slide-title"><h2>Заголовок</h2></div>
+                    <div class="main_slide-subtitle">Подзаголовок</div>
+                    <div class="main_slide-description">Описание</div>
+                    <a class="main_slide-btn btn" href="#">Подробнее</a>
+                </div>
+                <div class="main_slide-right">
+                    Форма связи
+                </div>
+            </div>            
+        </div>
+
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="section_title">
+            <h2>Наши цены</h2>
+        </div>    
+        <div class="main_price-city">
+            <div class="city-title">
+                <h3>Ростов</h3>
+            </div>
+            </div class="main_price-table-slider">
+
+                <div class="price_slider-item-apparts">
+
+                    <div class="price_item-header">
+                        <div class="price_item-title">Квартира</div>
+                        <div class="price_item-subtitle subtitle">Поверка счетчика</div>
+                    </div>
+
+                    <div class="price_item-body">
+                        <div class="price_item-price">
+                            <div class="ruble_symbol">₽</div>
+                            <div class="price">550</div>
+                        </div>
+                        <div class="price_item-advantages">
+                            <div class="advantages_icon"></div>
+                            <div class="advantages_text"></div>
+                        </div>                        
+                    </div>
+
+                    <div class="price_item-btn">
+                        <a class="btn" href="#">Оставить заявку</a>
+                    </div>
+                    
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
 <?php get_footer(); ?>
