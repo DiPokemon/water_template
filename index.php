d2e1241bd946bb2e926f38f4f3714e1a48e4dfff
DiPokemon <dimitrnklnk@gmail.com>
2022-11-14 01:14:38 +0300

    add price table

--- index.php
+++ index.php
@@ -31,28 +31,79 @@
             </div>
             </div class="main_price-table-slider">
 
-                <div class="price_slider-item-apparts">
-
-                    <div class="price_item-header">
-                        <div class="price_item-title">Квартира</div>
-                        <div class="price_item-subtitle subtitle">Поверка счетчика</div>
-                    </div>
-
-                    <div class="price_item-body">
-                        <div class="price_item-price">
-                            <div class="ruble_symbol">₽</div>
-                            <div class="price">550</div>
+                <div class="price_slider-item apparts">
+                    <div class="price_slider-item-top">
+                        <div class="price_item-header">
+                            <div class="price_item-title">Квартира</div>
+                            <div class="price_item-subtitle subtitle">Поверка счетчика</div>
                         </div>
-                        <div class="price_item-advantages">
-                            <div class="advantages_icon"></div>
-                            <div class="advantages_text"></div>
-                        </div>                        
-                    </div>
 
-                    <div class="price_item-btn">
-                        <a class="btn" href="#">Оставить заявку</a>
+                        <div class="price_item-body">
+                            <div class="price_item-price">
+                                <div class="ruble_symbol">₽</div>
+                                <div class="price">550</div>
+                            </div>
+                            <div class="price_item-advantages">
+                                <div class="advantages_icon">0</div>
+                                <div class="advantages_text">Льгота</div>
+                            </div>                        
+                        </div>
                     </div>
-                    
+                    <div class="price_slider-item-bottom">
+                        <div class="price_item-btn">
+                            <a class="btn" href="#">Оставить заявку</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="price_slider-item house">
+                    <div class="price_slider-item-top">
+                        <div class="price_item-header">
+                            <div class="price_item-title">Дом</div>
+                            <div class="price_item-subtitle subtitle">Поверка счетчика</div>
+                        </div>
+
+                        <div class="price_item-body">
+                            <div class="price_item-price">
+                                <div class="ruble_symbol">₽</div>
+                                <div class="price">550</div>
+                            </div>
+                            <div class="price_item-advantages">
+                                <div class="advantages_icon"></div>
+                                <div class="advantages_text"></div>
+                            </div>                        
+                        </div>
+                    </div>
+                    <div class="price_slider-item-bottom">
+                        <div class="price_item-btn">
+                            <a class="btn" href="#">Оставить заявку</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="price_slider-item well">
+                    <div class="price_slider-item-top">
+                        <div class="price_item-header">
+                            <div class="price_item-title">Квартира</div>
+                            <div class="price_item-subtitle subtitle">Поверка счетчика</div>
+                        </div>
+
+                        <div class="price_item-body">
+                            <div class="price_item-price">
+                                <div class="ruble_symbol">₽</div>
+                                <div class="price">550</div>
+                            </div>
+                            <div class="price_item-advantages">
+                                <div class="advantages_icon"></div>
+                                <div class="advantages_text"></div>
+                            </div>                        
+                        </div>
+                    </div>
+                    <div class="price_slider-item-bottom">
+                        <div class="price_item-btn">
+                            <a class="btn" href="#">Оставить заявку</a>
+                        </div>
+                    </div>
                 </div>
 
             </div>
