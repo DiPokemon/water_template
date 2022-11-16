d2f683663fdd9226736dfdf0aa6fe700754c8ea6
DiPokemon <dimitrnklnk@gmail.com>
2022-11-16 19:47:12 +0300

    add blocks

--- static/js/init_sliders.js
+++ static/js/init_sliders.js
@@ -13,5 +13,21 @@ $(document).ready(function () {
                     }
                 },                
             ],
-        });        
+        });      
+    
+        $('.main_price-collect-slider').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },                
+            ],
+        });    
 });
