2eb4155ce867c559792f4d079566b96bba4541fe
Evgenii Kovalenko <kov.evgenii5@gmail.com>
2022-11-17 20:17:22 +0100

    footer begin

--- footer.php
+++ footer.php
@@ -1,7 +1,35 @@
         </main>
         
         <footer>
-
+          <div class="footer-top">
+             <div class="container">
+                <div class="footer-top__body">
+                  <div class="footer-top__img"></div>
+                  <div class="footer-top__list">
+                    <nav>
+                        <ul>
+                            <li>поверка счетчика воды</li>
+                            <li>установка счетчиков воды</li>
+                            <li>замена счетчиков воды</li>
+                            <li>о компании</li>
+                        </ul>    
+                    </nav> 
+                  </div>
+                  <div class="footer-top__telefon">
+                     <div class="footer-top__telefon1"><a href="tel:+79001280404">+7 (900) 128-04-04</a></div>
+                     <div class="footer-top__telefon2"><a href="tel:+79001281414">+7 (900) 128-14-14</a></div>
+                  </div>
+                </div>
+              </div>
+          </div>
+          <div class="footer-bottom">
+             <div class="container">
+                <div class="footer-bottom__body">
+                   <div class="footer-bottom__inf_left">© 2020. ИП Григорьянц Александр Георгиевич</div>
+                   <div class="footer-bottom__inf_right">Политика конфиденциальности</div>
+                </div>
+            </div>  
+          </div>
         </footer>
 
     </body>
