59d5d6ad11d34e613e9774ad52c6a84f2508f8e1
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2023-04-02 14:45:45 +0300

    lazyload for main slider bg

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,18 @@ $('.header__burger').click(function (event) {
     $('.header__burger,.header__menu').toggleClass('active');
     $('body').toggleClass('lock')
 }); 
+
+var lazyBgImg = function( el ) {
+    var lazyBgImgs = $(el).find('.lazy-bg-img');
+    lazyBgImgs.each(function(i) {
+        var el = $(this);
+        if ( !el.hasClass('is-lazy-loaded') ) {
+            el.addClass('is-lazy-loaded');
+        }
+    });
+}
+s.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    if ( currentSlide !== nextSlide ) {
+        lazyBgImg( $(slick.$slides.get(nextSlide)) );
+    }
+});
